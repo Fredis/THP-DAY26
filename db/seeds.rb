36f1ecb9a67ccdf901b0d9f3c8dc84035da519f9
Fredis <fredericstojicevic@gmail.com>
2018-11-06 00:48:22 +0100

    Add the require faker to seed in case of.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 5.times do |element|
 	users = User.create!(prenom: Faker::Artist.name, nom: Faker::Ancient.god, email: Faker::Internet.email, password: Faker::Internet.password(10, 20))
 end
